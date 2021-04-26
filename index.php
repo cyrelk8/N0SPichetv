6bd121c858e18f7cf7e27a6c461b1984dc087211
cyrelk8 <toupinglu61291@yeah.net>
2021-04-26 15:17:22 +0800

    finishing
    finishing

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
 <body>
 	<button id="get-btn">Get Data</button>
 	<button id="post-btn">Post Data</button>
+	<!-- using xhr -->
+	<!-- <script type="text/javascript" src="js/xhr.js" defer></script> -->
+
+	<!-- using fetch -->
+	<!-- <script type="text/javascript" src="js/fetch.js" defer></script> -->
+
 	<!-- using axios -->
 	<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 	<script type="text/javascript" src="js/axios.js" defer></script>
